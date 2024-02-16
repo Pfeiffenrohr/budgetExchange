72ee3c26082819ae1fc35548b933599c5e2c919c
Pfeiffenrohr <44580842+Pfeiffenrohr@users.noreply.github.com>
2024-02-16 13:12:22 +0100

    test checkin

--- scripts/budgetExchange.sh
+++ scripts/budgetExchange.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cd /var/lib/budgetserver
+sleep 3600
 /usr/bin/java -jar /var/lib/budgetserver/budgetexchange.jar --budgetserverhost=$budgetserverhost --budgetserverport=$budgetserverport
