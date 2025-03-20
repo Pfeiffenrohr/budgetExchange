8a2fb5af12b49aae071db9c1b40e92a0e5cfd951
Pfeiffenrohr <44580842+Pfeiffenrohr@users.noreply.github.com>
2025-03-20 13:40:20 +0100

    CompareTest

---
+++ src/test/java/cam/lechner/budgetexchange/application/CompareTest.java
@@ -0,0 +1,34 @@
+package cam.lechner.budgetexchange.application;
+
+
+import cam.lechner.budgetexchange.apicall.ApiCall;
+import cam.lechner.budgetexchange.cospend.BillRespond;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import static org.mockito.ArgumentMatchers.anyString;
+
+@ExtendWith(MockitoExtension.class)
+@Disabled
+public class CompareTest {
+   /* @Mock
+    private RestTemplate restTemplate;
+*/
+    @InjectMocks
+    private ApiCall apiCall = new ApiCall();
+
+    @Test
+    void compareCospendBudgetTest() {
+        String project = "budgetall";
+        BillRespond respond = new BillRespond();
+        Mockito.when(apiCall.getAllBills(Mockito.anyString()))
+                .thenReturn(respond);
+        Mockito.when(apiCall.deleteBill(anyString(),anyString())).thenReturn(true);
+        Compare compare = new Compare();
+        compare.compareCospendBudget(project);
+    }
+
+}
