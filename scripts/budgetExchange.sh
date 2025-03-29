1d49f1203802d1dad9b979789bbd3661c2d434d7
richard lechner <richard@richardlechner.de>
2025-03-29 17:11:11 +0100

    Anpassung an Strato

--- scripts/budgetExchange.sh
+++ scripts/budgetExchange.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /var/lib/budgetserver
 sleep 20
-/opt/java/openjdk/bin/java -jar /var/lib/budgetserver/budgetexchange.jar --budgetserverhost=$budgetserverhost --budgetserverport=$budgetserverport
+/opt/java/openjdk/bin/java -jar /var/lib/budgetserver/budgetexchange.jar --budgetserverhost=$budgetserverhost --budgetserverport=$budgetserverport --databaseurl=$databaseurl
