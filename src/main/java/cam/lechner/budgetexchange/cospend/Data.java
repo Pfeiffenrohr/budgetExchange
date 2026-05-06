8086f028e71fe32b76668f6e580dc596e31780db
Richard Lechner <lechnerri@mediamarktsaturn.com>
2026-05-06 15:13:25 +0200

    Fixed nextcloudpassword again

--- src/main/java/cam/lechner/budgetexchange/cospend/Data.java
+++ src/main/java/cam/lechner/budgetexchange/cospend/Data.java
@@ -4,12 +4,26 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.Arrays;
+
 @Getter
 @Setter
 @NoArgsConstructor
 public class Data {
-    private Integer nb_bills;
-    private Integer [] allBillIds;
-    private Integer timestamp;
-    private Bills [] bills;
+    private Bill[] bills;
+
+    public Bill[] getBills() {
+        return bills;
+    }
+
+    public void setBills(Bill[] bills) {
+        this.bills = bills;
+    }
+
+    public Integer[] getAllBillIds() {
+        if (bills == null) return new Integer[0];
+        return Arrays.stream(bills)
+                .map(Bill::getId)
+                .toArray(Integer[]::new);
+    }
 }
